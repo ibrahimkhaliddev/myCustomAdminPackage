64169b23227dded6918c1c6408db9b9fea0a7ba3
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-28 17:36:04 +0500

    done

---
+++ config/MyCustomPackage.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'controller' => [
+        'name' => 'baseController',
+        'namespace' => 'MyCustomPackage\Http\Controllers',
+    ],
+    
+    'view' => [
+        'name' => 'layout.blade.php',
+        'path' => resource_path('views/vendor/my-package/layout.blade.php'),
+    ],
+
+];
