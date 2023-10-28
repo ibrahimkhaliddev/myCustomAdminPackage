6f0884b9e9640078a844412d5678939a2bb3b0db
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-28 17:48:11 +0500

    Create MyPackageServiceProvider.php

---
+++ src/Providers/MyPackageServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MyPackage\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MyPackageServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // ...
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/MyCustomPackage.php' => config_path('MyCustomPackage.php'),
+            __DIR__ . '/../resources/views' => resource_path('views/MyCustomPackage'),
+        ]);
+    }
+}
