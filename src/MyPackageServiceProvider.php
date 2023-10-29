e5afc57d504ca58855706654bb40e0d0c06f2b0e
MianIbrahimKhalid <ibrahimkhalid.chs@gmail.com>
2023-10-29 22:51:31 +0500

    Update MyPackageServiceProvider.php

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -14,8 +14,8 @@ class MyPackageServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '../config/MyCustomPackage.php' => config_path('MyCustomPackage.php'),
-            __DIR__ . '../resources/views' => resource_path('views/MyCustomPackage'),
+            __DIR__ . '/../config/MyCustomPackage.php' => config_path('MyCustomPackage.php'),
+            __DIR__ . '/../src/resources/views/myCustomAdminPackage/layout.blade.php' => resource_path('views/MyCustomPackage'),
         ]);
     }
 }
